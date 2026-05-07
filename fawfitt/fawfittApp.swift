e3503482753fc4bf7ea064c09d21297ad8977561
fawwaz2828 <fawwazwibowo96@gmail.com>
2026-05-07 16:14:24 +0800

    Initial Commit

---
+++ fawfitt/fawfittApp.swift
@@ -0,0 +1,17 @@
+//
+//  fawfittApp.swift
+//  fawfitt
+//
+//  Created by 33 on 2026/5/7.
+//
+
+import SwiftUI
+
+@main
+struct fawfittApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
